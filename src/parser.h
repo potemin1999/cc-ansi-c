aab8c270824e708e1cf8d52a7c4da4f3534c63ef
potemin1999 <potemin1999@bk.ru>
2019-10-11 16:59:15 +0300

    + types of ast nodes

--- src/parser.h
+++ src/parser.h
@@ -8,9 +8,42 @@
 #ifndef CC_LABS_PARSER_H
 #define CC_LABS_PARSER_H
 
-#endif //CC_LABS_PARSER_H
+#define TRANSLATION_UNIT        100
+#define EXTERNAL_DECLARATION    101
+#define FUNCTION_DEFINITION     102
+#define COMPOUND_STATEMENT      110
+#define ITERATION_STATEMENT     111
+#define SELECTION_STATEMENT     112
+#define LABELED_STATEMENT       113
+#define JUMP_STATEMENT          114
+#define EXPRESSION_STATEMENT    115
+#define STATEMENT_LIST          120
+#define STATEMENT               116
+#define TYPE_NAME               121
+#define PARAMETER_TYPE_LIST     122
+#define PARAMETER_LIST          123
+#define PARAMETER_DECLARATION   124
+#define STRUCT_DECLARATION_LIST 125
+#define STRUCT_DECLARATION      126
+#define STRUCT_DECLARATOR_LIST  127
+#define STRUCT_DECLARATOR       128
+#define DECLARATION_LIST        129
+#define DECLARATION             134
+#define DECLARATION_SPECIFIER   135
+#define DECLARATION_SPECIFIERS  136
+#define INIT_DECLARATOR_LIST    137
+#define INIT_DECLARATOR         138
+#define ABSTRACT_DECLARATOR     139
+#define DIRECT_ABSTRACT_DECLARATOR  140
+#define DIRECT_DECLARATOR       141
+#define DECLARATOR              142
+#define POINTER                 143
 
-struct AstNode{};
+struct AstNode {
+    int type{};
+
+    explicit AstNode(int type) : type(type) {}
+};
 
 struct PrimaryExpression;
 
@@ -98,11 +131,12 @@ struct ConditionalExpression {
 struct Pointer;
 struct DirectDeclarator;
 
-struct Declarator: AstNode {
-    Pointer *pointer;
-    DirectDeclarator *directDeclarator;
+struct Declarator : AstNode {
+    Pointer *pointer{};
+    DirectDeclarator *directDeclarator{};
 
     Declarator(Pointer *pointer, DirectDeclarator *declarator) :
+            AstNode(DECLARATOR),
             pointer(pointer), directDeclarator(declarator) {}
 };
 
@@ -110,82 +144,102 @@ struct ParameterTypeList;
 struct IdentifierList;
 struct Constant;
 
-struct DirectDeclarator : AstNode{
+struct DirectDeclarator : AstNode {
     char *identifier{};
     DirectDeclarator *directDeclarator{};
-    union {
-        ParameterTypeList *parameterTypeList;
-        IdentifierList *identifierList{};
-        Constant *constant;
-    };
+    ParameterTypeList *parameterTypeList{};
+    IdentifierList *identifierList{};
+    Constant *constant{};
 
-    explicit DirectDeclarator(char *identifier) : identifier(identifier) {}
+    explicit DirectDeclarator(char *identifier) :
+            AstNode(DIRECT_DECLARATOR),
+            identifier(identifier) {}
 
-    explicit DirectDeclarator(DirectDeclarator *directDeclarator) : directDeclarator(directDeclarator) {}
+    explicit DirectDeclarator(DirectDeclarator *directDeclarator) :
+            AstNode(DIRECT_DECLARATOR),
+            directDeclarator(directDeclarator) {}
 
-    DirectDeclarator(DirectDeclarator *directDeclarator, ParameterTypeList *parameterTypeList) : directDeclarator(
-            directDeclarator), parameterTypeList(parameterTypeList) {}
+    DirectDeclarator(DirectDeclarator *directDeclarator, ParameterTypeList *parameterTypeList) :
+            AstNode(DIRECT_DECLARATOR),
+            directDeclarator(directDeclarator), parameterTypeList(parameterTypeList) {}
 
-    DirectDeclarator(DirectDeclarator *directDeclarator, IdentifierList *identifierList) : directDeclarator(
-            directDeclarator), identifierList(identifierList) {}
+    DirectDeclarator(DirectDeclarator *directDeclarator, IdentifierList *identifierList) :
+            AstNode(DIRECT_DECLARATOR),
+            directDeclarator(directDeclarator), identifierList(identifierList) {}
 
-    DirectDeclarator(DirectDeclarator *directDeclarator, Constant *constant) : directDeclarator(
-            directDeclarator), constant(constant) {}
+    DirectDeclarator(DirectDeclarator *directDeclarator, Constant *constant) :
+            AstNode(DIRECT_DECLARATOR),
+            directDeclarator(directDeclarator), constant(constant) {}
 };
 
 struct AbstractDeclarator;
 
-struct DirectAbstractDeclarator : AstNode{
+struct DirectAbstractDeclarator : AstNode {
     Constant *constant{};
     ParameterTypeList *parameterTypeList{};
     AbstractDeclarator *abstractDeclarator{};
     DirectAbstractDeclarator *directAbstractDeclarator{};
 
-    DirectAbstractDeclarator() = default;
+    DirectAbstractDeclarator() : AstNode(DIRECT_ABSTRACT_DECLARATOR) {};
 
-    explicit DirectAbstractDeclarator(Constant *constant) : constant(constant) {}
+    explicit DirectAbstractDeclarator(Constant *constant) :
+            AstNode(DIRECT_ABSTRACT_DECLARATOR),
+            constant(constant) {}
 
-    DirectAbstractDeclarator(ParameterTypeList *parameterTypeList) : parameterTypeList(parameterTypeList) {}
+    explicit DirectAbstractDeclarator(ParameterTypeList *parameterTypeList) :
+            AstNode(DIRECT_ABSTRACT_DECLARATOR),
+            parameterTypeList(parameterTypeList) {}
 
-    DirectAbstractDeclarator(AbstractDeclarator *abstractDeclarator) : abstractDeclarator(abstractDeclarator) {}
+    explicit DirectAbstractDeclarator(AbstractDeclarator *abstractDeclarator) :
+            AstNode(DIRECT_ABSTRACT_DECLARATOR),
+            abstractDeclarator(abstractDeclarator) {}
 
-    DirectAbstractDeclarator(DirectAbstractDeclarator *directAbstractDeclarator, Constant *constant)
-            : directAbstractDeclarator(directAbstractDeclarator), constant(constant) {}
+    DirectAbstractDeclarator(DirectAbstractDeclarator *directAbstractDeclarator, Constant *constant) :
+            AstNode(DIRECT_ABSTRACT_DECLARATOR),
+            directAbstractDeclarator(directAbstractDeclarator), constant(constant) {}
 
-    DirectAbstractDeclarator(DirectAbstractDeclarator *directAbstractDeclarator, ParameterTypeList *parameterTypeList)
-            : directAbstractDeclarator(directAbstractDeclarator), parameterTypeList(parameterTypeList) {}
+    DirectAbstractDeclarator(DirectAbstractDeclarator *directAbstractDeclarator, ParameterTypeList *parameterTypeList) :
+            AstNode(DIRECT_ABSTRACT_DECLARATOR),
+            directAbstractDeclarator(directAbstractDeclarator), parameterTypeList(parameterTypeList) {}
 
 };
 
-struct AbstractDeclarator : AstNode{
+struct AbstractDeclarator : AstNode {
     Pointer *pointer{};
     DirectAbstractDeclarator *directAbstractDeclarator{};
 
-    explicit AbstractDeclarator(Pointer *pointer) : pointer(pointer) {}
+    explicit AbstractDeclarator(Pointer *pointer) :
+            AstNode(ABSTRACT_DECLARATOR),
+            pointer(pointer) {}
 
-    explicit AbstractDeclarator(DirectAbstractDeclarator *directAbstractDeclarator1) : directAbstractDeclarator(
-            directAbstractDeclarator) {}
+    explicit AbstractDeclarator(DirectAbstractDeclarator *directAbstractDeclarator) :
+            AstNode(ABSTRACT_DECLARATOR),
+            directAbstractDeclarator(directAbstractDeclarator) {}
 
     AbstractDeclarator(Pointer *pointer, DirectAbstractDeclarator *directAbstractDeclarator) :
+            AstNode(ABSTRACT_DECLARATOR),
             pointer(pointer), directAbstractDeclarator(directAbstractDeclarator) {}
 };
 
 struct Initializer;
 
-struct InitDeclarator : AstNode{
-    Declarator *declarator;
-    Initializer *initializer;
+struct InitDeclarator : AstNode {
+    Declarator *declarator{};
+    Initializer *initializer{};
 
-    explicit InitDeclarator(Declarator *declarator) : declarator(declarator) {}
+    explicit InitDeclarator(Declarator *declarator) :
+            AstNode(INIT_DECLARATOR),
+            declarator(declarator) {}
 
     InitDeclarator(Declarator *declarator, Initializer *initializer)
-            : declarator(declarator), initializer(initializer) {}
+            : AstNode(INIT_DECLARATOR),
+              declarator(declarator), initializer(initializer) {}
 };
 
-struct InitDeclaratorList {
+struct InitDeclaratorList : AstNode {
     std::list<InitDeclarator *> declaratorList;
 
-    explicit InitDeclaratorList(InitDeclarator *declarator) {
+    explicit InitDeclaratorList(InitDeclarator *declarator) : AstNode(INIT_DECLARATOR_LIST) {
         this->addDeclarator(declarator);
     }
 
@@ -198,23 +252,29 @@ struct StorageClassSpecifier;
 struct TypeSpecifier;
 struct TypeQualifier;
 
-struct DeclarationSpecifier : AstNode{
-    StorageClassSpecifier *storageClassSpecifier;
-    TypeSpecifier *typeSpecifier;
-    TypeQualifier *typeQualifier;
+struct DeclarationSpecifier : AstNode {
+    StorageClassSpecifier *storageClassSpecifier{};
+    TypeSpecifier *typeSpecifier{};
+    TypeQualifier *typeQualifier{};
 
     explicit DeclarationSpecifier(StorageClassSpecifier *storageClassSpecifier) :
+            AstNode(DECLARATION_SPECIFIER),
             storageClassSpecifier(storageClassSpecifier) {}
 
-    explicit DeclarationSpecifier(TypeSpecifier *typeSpecifier) : typeSpecifier(typeSpecifier) {}
+    explicit DeclarationSpecifier(TypeSpecifier *typeSpecifier) :
+            AstNode(DECLARATION_SPECIFIER),
+            typeSpecifier(typeSpecifier) {}
 
-    explicit DeclarationSpecifier(TypeQualifier *typeQualifier) : typeQualifier(typeQualifier) {}
+    explicit DeclarationSpecifier(TypeQualifier *typeQualifier) :
+            AstNode(DECLARATION_SPECIFIER),
+            typeQualifier(typeQualifier) {}
 };
 
-struct DeclarationSpecifiers: AstNode {
+struct DeclarationSpecifiers : AstNode {
     std::list<DeclarationSpecifier *> declarationSpecifierList;
 
-    explicit DeclarationSpecifiers(DeclarationSpecifier *declarationSpecifier) {
+    explicit DeclarationSpecifiers(DeclarationSpecifier *declarationSpecifier) :
+            AstNode(DECLARATION_SPECIFIERS) {
         addDeclarationSpecifier(declarationSpecifier);
     }
 
@@ -223,20 +283,24 @@ struct DeclarationSpecifiers: AstNode {
     }
 };
 
-struct Declaration : AstNode{
+struct Declaration : AstNode {
     DeclarationSpecifiers *declarationSpecifiers;
     InitDeclaratorList *initDeclaratorList{};
 
-    explicit Declaration(DeclarationSpecifiers *declarationSpecifiers) : declarationSpecifiers(declarationSpecifiers) {}
+    explicit Declaration(DeclarationSpecifiers *declarationSpecifiers) :
+            AstNode(DECLARATION),
+            declarationSpecifiers(declarationSpecifiers) {}
 
     Declaration(DeclarationSpecifiers *declarationSpecifiers, InitDeclaratorList *initDeclaratorList) :
+            AstNode(DECLARATION),
             declarationSpecifiers(declarationSpecifiers), initDeclaratorList(initDeclaratorList) {}
 };
 
-struct DeclarationList : AstNode{
+struct DeclarationList : AstNode {
     std::list<Declaration *> declarationList;
 
-    explicit DeclarationList(Declaration *declaration) {
+    explicit DeclarationList(Declaration *declaration) :
+            AstNode(DECLARATION_LIST) {
         addDeclaration(declaration);
     }
 
@@ -245,22 +309,25 @@ struct DeclarationList : AstNode{
     }
 };
 
-struct StructDeclarator : AstNode{
+struct StructDeclarator : AstNode {
     Declarator *declarator{};
     Constant *constant{};
 
-    explicit StructDeclarator(Declarator *declarator) : declarator(declarator) {}
+    explicit StructDeclarator(Declarator *declarator) :
+            AstNode(STRUCT_DECLARATOR), declarator(declarator) {}
 
-    explicit StructDeclarator(Constant *constant) : constant(constant) {}
+    explicit StructDeclarator(Constant *constant) :
+            AstNode(STRUCT_DECLARATOR), constant(constant) {}
 
     StructDeclarator(Declarator *declarator, Constant *constant) :
-            declarator(declarator), constant(constant) {}
+            AstNode(STRUCT_DECLARATOR), declarator(declarator), constant(constant) {}
 };
 
-struct StructDeclaratorList: AstNode {
+struct StructDeclaratorList : AstNode {
     std::list<StructDeclarator *> structDeclaratorList;
 
-    explicit StructDeclaratorList(StructDeclarator *structDeclarator) {
+    explicit StructDeclaratorList(StructDeclarator *structDeclarator) :
+            AstNode(STRUCT_DECLARATOR_LIST) {
         addStructDeclarator(structDeclarator);
     }
 
@@ -277,12 +344,15 @@ struct StructDeclaration : AstNode {
     StructDeclaratorList *structDeclaratorList;
 
     StructDeclaration(SpecifierQualifierList *specifierQualifierList, StructDeclaratorList *structDeclaratorList) :
+            AstNode(STRUCT_DECLARATION),
             specifierQualifierList(specifierQualifierList), structDeclaratorList(structDeclaratorList) {}
 };
 
 struct StructDeclarationList : AstNode {
-    std::list<StructDeclaration> structDeclarationList;
-    explicit StructDeclarationList(StructDeclaration structDeclaration) {
+    std::list<StructDeclaration *> structDeclarationList;
+
+    explicit StructDeclarationList(StructDeclaration *structDeclaration) :
+            AstNode(STRUCT_DECLARATION_LIST) {
         addStructDeclaration(structDeclaration);
     }
 
@@ -300,13 +370,16 @@ struct ParameterDeclaration : AstNode {
     AbstractDeclarator *abstractDeclarator;
     Declarator *declarator;
 
-    explicit ParameterDeclaration(DeclarationSpecifiers *declarationSpecifiers) : declarationSpecifiers(
-            declarationSpecifiers) {}
+    explicit ParameterDeclaration(DeclarationSpecifiers *declarationSpecifiers) :
+            AstNode(PARAMETER_DECLARATION),
+            declarationSpecifiers(declarationSpecifiers) {}
 
     ParameterDeclaration(DeclarationSpecifiers *declarationSpecifiers, Declarator *declarator) :
+            AstNode(PARAMETER_DECLARATION),
             declarationSpecifiers(declarationSpecifiers), declarator(declarator) {}
 
     ParameterDeclaration(DeclarationSpecifiers *declarationSpecifiers, AbstractDeclarator *abstractDeclarator) :
+            AstNode(PARAMETER_DECLARATION),
             declarationSpecifiers(declarationSpecifiers), abstractDeclarator(abstractDeclarator) {}
 
 };
@@ -314,7 +387,7 @@ struct ParameterDeclaration : AstNode {
 struct ParameterList : AstNode {
     std::list<ParameterDeclaration *> parameterDeclarationList;
 
-    explicit ParameterList(ParameterDeclaration *parameterDeclaration) {
+    explicit ParameterList(ParameterDeclaration *parameterDeclaration) : AstNode(PARAMETER_LIST) {
         addParameterDeclaration(parameterDeclaration);
     }
 
@@ -328,65 +401,68 @@ struct ParameterTypeList : AstNode {
     bool withEllipsis;
 
     explicit ParameterTypeList(ParameterList *parameterList, bool withEllipsis) :
-            parameterList(parameterList), withEllipsis(withEllipsis) {}
+            AstNode(PARAMETER_TYPE_LIST),
+            parameterList(parameterList),
+            withEllipsis(withEllipsis) {}
 
-  
+};
+
 struct Declaration;
 
-struct TypeName{
+struct TypeName {
     SpecifierQualifierList specifierQualifierList;
     AbstractDeclarator declarator{};
 
     // TODO Fix initialization
     TypeName(SpecifierQualifierList list) :
-        specifierQualifierList(list){};
+            specifierQualifierList(list) {};
 
     // TODO Fix initialization
     TypeName(SpecifierQualifierList list, AbstractDeclarator decl) :
-        specifierQualifierList(list), declarator(decl){};
+            specifierQualifierList(list), declarator(decl) {};
 };
 
-struct Statement{
+struct Statement {
 };
 
-struct ExpressionStatement : Statement{
+struct ExpressionStatement : Statement {
     Expression expression{};
 };
 
-struct StatementList{
+struct StatementList {
     std::list<Statement> statements;
 };
 
 
-struct JumpStatement : Statement{
+struct JumpStatement : Statement {
     int keyword;
-    char* identifier;
+    char *identifier;
     Expression expression;
 };
 
 // TODO: keyword?
-struct LabeledStatement : Statement{
-    char* identifier{};
+struct LabeledStatement : Statement {
+    char *identifier{};
     Constant constant{};
     Statement statement;
 };
 
 // TODO keyword?
-struct SelectionStatement : Statement{
+struct SelectionStatement : Statement {
     Expression expression;
     Statement statement1;
     Statement statement2;
 };
 
 // TODO keyword?
-struct IterationStatement : Statement{
+struct IterationStatement : Statement {
 
     Expression expression;
     Statement statement;
     ExpressionStatement
 };
 
-struct CompoundStatement : Statement{
+struct CompoundStatement : Statement {
     StatementList statementList;
     DeclarationList declarationList;
 };
@@ -399,30 +475,34 @@ struct FunctionDefinition {
     DeclarationList declarationList{};
 
     FunctionDefinition(Declarator decl, CompoundStatement statement) :
-        declarator(decl), compoundStatement(statement){};
+            declarator(decl), compoundStatement(statement) {};
 
     FunctionDefinition(Declarator decl, DeclarationList declList, CompoundStatement statement) :
-        declarator(decl), declarationList(declList), compoundStatement(statement){};
+            declarator(decl), declarationList(declList), compoundStatement(statement) {};
 
     FunctionDefinition(DeclarationSpecifiers specifiers, Declarator decl, CompoundStatement statement) :
-        declarator(decl), declarationSpecifiers(specifiers), compoundStatement(statement){};
+            declarator(decl), declarationSpecifiers(specifiers), compoundStatement(statement) {};
 
-    FunctionDefinition(DeclarationSpecifiers specifiers, Declarator decl, DeclarationList declList, CompoundStatement statement) :
-        declarator(decl), declarationSpecifiers(specifiers), declarationList(declList), compoundStatement(statement){};
+    FunctionDefinition(DeclarationSpecifiers specifiers, Declarator decl, DeclarationList declList,
+                       CompoundStatement statement) :
+            declarator(decl), declarationSpecifiers(specifiers), declarationList(declList),
+            compoundStatement(statement) {};
 };
 
-struct ExternalDeclaration : AstNode{
+struct ExternalDeclaration : AstNode {
     Declaration declaration{};
     FunctionDefinition funcDefinition{};
 
     ExternalDeclaration(Declaration decl) :
-        declaration(decl){}
+            declaration(decl) {}
 
     ExternalDeclaration(FunctionDefinition definition) :
-        funcDefinition(definition){}
+            funcDefinition(definition) {}
 };
 
-struct TranslationUnit : AstNode{
+struct TranslationUnit : AstNode {
     std::list<ExternalDeclaration> declarations;
 
 };
+
+#endif //CC_LABS_PARSER_H
